557eda5e3e2bbb805b7b627446b4c4516f9694d1
safi-siddiqui-github <safisiddiqui.work@gmail.com>
2025-03-13 00:33:15 +0500

    initial-setup

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+
+export default function Home() {
+  return (
+    <div className="p-5 flex flex-col gap-5">
+      <h2 className="text-xl font-semibold">Next JS App</h2>
+      <hr />
+
+      <p className="">
+        Features
+      </p>
+      <p>
+        Production
+      </p>
+    </div>
+  );
+}
---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+
+  // Docker Build
+  output: "standalone",
+
+};
+
+export default nextConfig;
